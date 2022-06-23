ca74d97a4bde73d5233038ac8cc4a25bb30ac4cb
Mark Martinez <mark@cyberia.tv>
2022-06-23 21:47:27 +0200

    Migrate devshells to this flake.

--- flake.nix
+++ flake.nix
@@ -3,19 +3,24 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
+    flake-utils.lib.eachDefaultSystem (system: {
+      overlays = {
+        Rex = import ./Rex.nix;
+        ansible = import ./ansible.nix;
+        misc = import ./misc.nix;
+      };
+      devShells = {
+        infrastructure = with self.nixpkgs.infrastructure;
+          mkShell {
+            packages =
+              [ myScriptingPython Rex molecule ansible ansible-lint yamllint ];
+          };
+      };
+      nixpkgs = {
+        infrastructure = import nixpkgs {
           inherit system;
-          overlays = (nixpkgs.lib.attrValues self.overlays.${system});
+          overlays = with self.overlays; [ Rex ansible ];
         };
-      in {
-        overlays = {
-          Rex = import ./Rex.nix;
-          ansible = import ./ansible.nix;
-          misc = import ./misc.nix;
-        };
-        devShell = with pkgs; mkShell { packages = [ Rex ]; };
-        nixpkgs = pkgs;
-      });
+      };
+    });
 }
